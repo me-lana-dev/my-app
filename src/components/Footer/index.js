bcc89c0e3c3351014232637aa2e6aec0bd277c55
Lana Kaliush <me.lana.dev@gmail.com>
2020-09-11 12:42:34 +0300

    CRLF to LF

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as Style from './style.module.css';
+
+function Footer() {
+  return (
+      <footer>
+      <p className={Style.c1}>Footer</p>
+      </footer>
+    );
+}
+
+export {Footer};
---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as Style from './style.module.css';
+
+function Header() {
+  return (
+      <header>
+      <p className={Style.c1}>Header</p>
+      </header>
+    );
+}
+
+export {Header};
